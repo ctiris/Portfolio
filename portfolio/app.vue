8c5af29915e97cb3b1b170c4a0a8c640fbaaf9d8
ctiris <48613879+ctiris@users.noreply.github.com>
2022-08-22 15:42:01 -0700

    Added menu bar

---
+++ portfolio/app.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="columns menu">
+      <div class="column">
+        <a href="/work">Iris Chang</a>
+      </div>
+      <div class="column menu-links">
+        <a href="/work">Work</a>
+        <a href="/me" id="me">Me</a>
+        <a href="/play">Play</a>
+      </div>
+      <div class="column"></div>
+
+
+      <!-- <div class="left">
+        <a href="/work">Iris Chang</a>
+      </div>
+
+      <div class="center">
+        <a href="/work">Work</a>
+        <a href="/me">Me</a>
+        <a href="/play">Play</a>
+      </div>
+
+      <div class="right"></div> -->
+    </div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+.menu {
+  /* display: flex;
+  flex-direction: row;
+  justify-content: center; */
+}
+
+.menu-links {
+  text-align: center;
+  
+}
+
+#me {
+  margin-left: 70px;
+  margin-right: 70px;
+}
+
+
+</style>
---
+++ portfolio/pages/work.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="long-div">
+        <p>hi</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.long-div {
+    border-width: 100px;
+    border-color:blue;
+    border: 10px;
+    border-style: solid;
+    padding-top: 1100px;
+    padding-bottom: 1100px;
+}
+
+p {
+    text-decoration: underline;
+}
+</style>
